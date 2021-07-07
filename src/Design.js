cce127d83d7293c73dd3cced55b53014ac9a4bb7
Juan Denipoti <juanidenipoti11@gmail.com>
2021-07-07 15:31:05 -0300

    calculator done

--- src/Design.js
+++ src/Design.js
@@ -1,50 +1,73 @@
-import { useEffect, useState } from "react";
+/* eslint-disable no-eval */
+import { useState } from "react";
+   
+function Design(){
+    let operations=[]
+    let [display, setDisplay] = useState("")
 
-function Design(props){
-
-    let[number, setNumber] = useState(props.initial || 0)
-
-    useEffect(function(){
-        setNumber(props.initial)
-    }, [props])
-
-
-
-
+    function buttonClick (props) {
+        let sign = props.target.value
+        setDisplay(display.concat(sign)) 
+    } 
+    function numberClick (props) {
+        let number = props.target.value
+        setDisplay(display.concat(number))
+        
+    }
+    function clearDisplay(){
+        setDisplay("")
+    }
+    function deleteLastChar(){
+        setDisplay(display.slice(0,display.length-1))
+    }
+    function resolveClick(){
+        try {
+            setDisplay(eval(display).toString())
+        } catch (error) {
+            clearDisplay()
+        }
+       }
+       
+       
     return(
         <div className="calculadora">
             <div className="display">
-                <p> {number}</p>
+               <input type="text" disabled className="display-number" value={display}></input>
             </div>
-            <table className="numbers">
+            <hr className="borderline"></hr>
+            <table className="table">
                 <tr className="row">
-                    <th><button>AC</button></th>
-                    <th><button >DEL</button></th>
-                    <th><button >/</button></th>
-                    <th><button >X</button></th>
+                    <th><button value="AC" onClick={clearDisplay} className="special-btn">AC</button></th>
+                    <th><button value="DEL" onClick={deleteLastChar} className="special-btn">DEL</button></th>
+                    <th><button value="/" onClick={buttonClick} className="special-btn">/</button></th>
+                    <th><button value="*" onClick={buttonClick} className="special-btn">*</button></th>
                 </tr>
                 <tr className="row">
-                    <th><button >7</button></th>
-                    <th><button>8</button></th>
-                    <th><button >9</button></th>
-                    <th><button >-</button></th>
+                    <th><button value="7" onClick={numberClick} className="number-btn">7</button></th>
+                    <th><button value="8" onClick={numberClick} className="number-btn">8</button></th>
+                    <th><button value="9" onClick={numberClick} className="number-btn">9</button></th>
+                    <th><button value="-" onClick={buttonClick} className="special-btn">-</button></th>
                 </tr>
                 <tr className="row">
-                    <th><button >4</button></th>
-                    <th><button >5</button></th>
-                    <th><button >6</button></th>
-                    <th><button >+</button></th>
+                    <th><button value="4" onClick={numberClick} className="number-btn">4</button></th>
+                    <th><button value="5" onClick={numberClick} className="number-btn">5</button></th>
+                    <th><button value="6" onClick={numberClick} className="number-btn">6</button></th>
+                    <th><button value="+" onClick={buttonClick} className="special-btn">+</button></th>
                 </tr>
                 <tr className="row">
-                    <th><button>1</button></th>
-                    <th><button >2</button></th>
-                    <th><button >3</button></th>
-                    <th><button>=</button></th>
+                    <th><button value="1" onClick={numberClick} className="number-btn">1</button></th>
+                    <th><button value="2" onClick={numberClick} className="number-btn">2</button></th>
+                    <th><button value="3" onClick={numberClick} className="number-btn">3</button></th>
+                    <th><button value="=" onClick={resolveClick} className="special-btn">=</button></th>
                 </tr>
                 <tr className="row">
-                    <th><button>0</button></th>
+                    <th><button value="0" onClick={numberClick} className="number-btn">0</button></th>
+                    <th><button value="." onClick={numberClick} className="number-btn">.</button></th>
+                    
                 </tr>
             </table>
+            <div className="history">
+            </div>            
         </div>
     )
 }
